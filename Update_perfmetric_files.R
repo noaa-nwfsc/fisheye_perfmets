980a466f3be6a0f7d6991eec99e9b75c554b310a
rhunter <raymond.hunter@noaa.gov>
2025-07-09 12:39:07 -0700

    initial commit

---
+++ Update_perfmetric_files.R
@@ -0,0 +1,37 @@
+# Display list of available date stamp folders for performance metrics
+dir("R:/Confidential/FISHEyE/data/PerformanceMetrics")
+
+# Copy files over for Performance Metrics
+# Note from 6/25/2024 - I only copied over the CP data file
+perfmetricsdir <- "R:/Confidential/FISHEyE/data/PerformanceMetrics/2025-03-17"
+#perfmetricsfiles <- list.files(perfmetricsdir)[grepl('RData', list.files(perfmetricsdir))]
+
+perfmetricsfiles <- list.files(perfmetricsdir)[grepl('perfmetrics.rds', list.files(perfmetricsdir))]
+
+try(if(length(perfmetricsfiles) == 0) stop("There aren't any files to move in that folder"))
+
+destination <- ifelse(grepl('PerformanceMetrics', getwd()), "data", "Performancemetrics/data")
+
+for(pfiles in perfmetricsfiles) {
+
+    file.copy(
+        from = file.path(perfmetricsdir, pfiles),
+        to = destination,
+        overwrite = T)
+
+}
+
+datvarsfiles <- list.files(perfmetricsdir)[grepl('datvars', list.files(perfmetricsdir))]
+
+try(if(length(datvarsfiles) == 0) stop("There aren't any datvars files to move in that folder"))
+
+for(dfiles in datvarsfiles) {
+
+    file.copy(
+        from = file.path(perfmetricsdir, dfiles),
+        to = destination,
+        overwrite = T)
+
+}
+
+
