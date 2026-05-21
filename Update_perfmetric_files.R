ba2670c2b6b2fee773e483704dd07660d792ede8
Erin Steiner <erin.steiner@noaa.gov>
2026-05-21 15:43:26 -0700

    Minor formatting edits to figures

--- Update_perfmetric_files.R
+++ Update_perfmetric_files.R
@@ -1,13 +1,18 @@
 library(here)
 # Display list of available date stamp folders for performance metrics
 pm_dir <- file.path(
-    "G:", "Shared drives", "NMFS NWC FRAM EDC CE (contains MSA Confidential Data)", "FISHEyE", "data", "PerformanceMetrics"
+    "G:",
+    "Shared drives",
+    "NMFS NWC FRAM EDC CE (contains MSA Confidential Data)",
+    "FISHEyE",
+    "data",
+    "PerformanceMetrics"
 )
 dir(pm_dir)
 
 # Copy files over for Performance Metrics
 # Note from 6/25/2024 - I only copied over the CP data file
-perfmetricsdir <- file.path(pm_dir, "2026-04-06")
+perfmetricsdir <- file.path(pm_dir, "2026-05-21")
 #perfmetricsfiles <- list.files(perfmetricsdir)[grepl('RData', list.files(perfmetricsdir))]
 
 perfmetricsfiles <- list.files(perfmetricsdir)[grepl(
