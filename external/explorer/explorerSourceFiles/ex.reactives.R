878394ec3c4e193b41afe6258dd930075b1120e7
Erin Steiner <erin.steiner@noaa.gov>
2026-05-27 16:02:52 -0700

    Remove extra conf flags from data table

--- external/explorer/explorerSourceFiles/ex.reactives.R
+++ external/explorer/explorerSourceFiles/ex.reactives.R
@@ -315,8 +315,11 @@ DatSubTable <- reactive({
 
     alwaysexclude <- c(
         'metric_flag',
+        'waiverfail_flag',
+        'conf_flag',
+        'vtype_conf',
         'conf',
-        'flag',
+        # 'flag',
         'unit',
         'ylab',
         'sort',
